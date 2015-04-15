1fafb81148fada30511226b95a024ef6687a0b5a
eshiho <shiho.elliptic@gmail.com>
2015-04-15 00:23:16 +0000

    [ADD] refactor setup.sh, add pyrc

--- setup.sh
+++ setup.sh
@@ -2,34 +2,24 @@
 
 SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
-if [ -L ~/.vimrc ]; then
-  rm ~/.vimrc
-fi
-if [ -L ~/.bashrc ]; then
-  rm ~/.bashrc
-fi
-if [ -L ~/opt ]; then
-  rm ~/opt
-fi
-if [ -L ~/.vim ]; then
-  rm ~/.vim
-fi
-if [ -f ~/.vimrc ]; then
-  mv ~/.vimrc ~/.vimrc.bak
-fi
-if [ -f ~/.bashrc ]; then
-  mv ~/.bashrc ~/.bashrc.bak
-fi
-if [ -d ~/.vim ]; then
-  mv ~/.vim ~/.vim.bak
-fi
-if [ -d ~/opt ]; then
-  mv ~/opt ~/opt.bak
-fi
+function entry () {
+  if [ "`echo \"$1\"|perl -p0e\"$\=m|/$|;}{\"`" = "1" ]; then
+    target=${1%/}
+  else
+    target=$1
+  fi
+  if [ -L "$HOME/$target" ]; then
+    rm "$HOME/$target"
+  fi
+  if [ -f "$HOME/$target" ]; then
+    mv "$HOME/$target" "$HOME/$target.bak"
+  fi
+  ln -s "$SCRIPT_PATH/$target" "$HOME/$target"
+}
 
-ln -s $SCRIPT_PATH/.vimrc ~/.vimrc
-ln -s $SCRIPT_PATH/.vim ~/.vim
-ln -s $SCRIPT_PATH/.bashrc ~/.bashrc
-ln -s $SCRIPT_PATH/opt ~/opt
+entry ".pyrc"
+entry ".vimrc"
+entry ".bashrc"
 
-
+entry ".vim/"
+entry "opt/"
